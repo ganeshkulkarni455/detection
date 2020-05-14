6ec6936616304fc462a645160715dd5b49ff18cd
ganeshkulkarni455 <64393872+ganeshkulkarni455@users.noreply.github.com>
2020-05-14 07:54:58 +0530

    Update train.py

--- train.py
+++ train.py
@@ -100,9 +100,10 @@ def main(args):
         collate_fn=utils.collate_fn)
 
     print("Creating model")
-    model = torchvision.models.detection.__dict__[args.model](num_classes=num_classes,
+    #model = torchvision.models.detection.__dict__[args.model](num_classes=num_classes,
                                                               pretrained=args.pretrained)
-    
+        
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
     in_features = model.roi_heads.box_predictor.cls_score.in_features
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
