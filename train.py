aa4371bd46fd7ed8980877a387b9e0438eec36fd
ganeshkulkarni455 <64393872+ganeshkulkarni455@users.noreply.github.com>
2020-05-19 22:36:06 +0530

    Update train.py

--- train.py
+++ train.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
                         help='number of total epochs to run')
     parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                         help='number of data loading workers (default: 4)')
-    parser.add_argument('--lr', default=0.0005, type=float,
+    parser.add_argument('--lr', default=0.02, type=float,
                         help='initial learning rate, 0.02 is the default value for training '
                         'on 8 gpus and 2 images_per_gpu')
     parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
