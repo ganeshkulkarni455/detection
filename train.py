cfe31de1a3c087c2a667c2d8bc2df3e7d9aa2b40
ganeshkulkarni455 <64393872+ganeshkulkarni455@users.noreply.github.com>
2020-05-20 00:34:24 +0530

    Update train.py

--- train.py
+++ train.py
@@ -157,8 +157,6 @@ def main(args):
         # evaluate after every epoch
         evaluate(model, data_loader_test, device=device)
         
-        torch.save(model.state_dict(), args.output_dir)
-        
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
