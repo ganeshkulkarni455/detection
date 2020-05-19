cd5226ee3852e412f6b35a799cab7566647fced7
ganeshkulkarni455 <64393872+ganeshkulkarni455@users.noreply.github.com>
2020-05-19 22:45:32 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -72,70 +72,70 @@ def get_names(json_file_path):
 
   return NAME, num_classes 
 
-class PennFudanDataset(torch.utils.data.Dataset):
-    def __init__(self, image_folder_path, json_file_path, transforms=None):
-        #self.root = root
-        self.image_folder_path = image_folder_path
-        self.mask_folder_path = json_file_path              #give mask folder directory as json for perticular this dataset
-        self.transforms = transforms
-        # load all image files, sorting them to
-        # ensure that they are aligned
-        self.imgs = list(sorted(os.listdir(image_folder_path)))
-        self.masks = list(sorted(os.listdir(json_file_path)))
+# class PennFudanDataset(torch.utils.data.Dataset):
+#     def __init__(self, image_folder_path, json_file_path, transforms=None):
+#         #self.root = root
+#         self.image_folder_path = image_folder_path
+#         self.mask_folder_path = json_file_path              #give mask folder directory as json for perticular this dataset
+#         self.transforms = transforms
+#         # load all image files, sorting them to
+#         # ensure that they are aligned
+#         self.imgs = list(sorted(os.listdir(image_folder_path)))
+#         self.masks = list(sorted(os.listdir(json_file_path)))
 
-    def __getitem__(self, idx):
-        # load images ad masks
-        img_path = os.path.join(self.image_folder_path, self.imgs[idx])
-        mask_path = os.path.join(self.mask_folder_path", self.masks[idx])
-        img = Image.open(img_path).convert("RGB")
-        # note that we haven't converted the mask to RGB,
-        # because each color corresponds to a different instance
-        # with 0 being background
-        mask = Image.open(mask_path)
+#     def __getitem__(self, idx):
+#         # load images ad masks
+#         img_path = os.path.join(self.image_folder_path, self.imgs[idx])
+#         mask_path = os.path.join(self.mask_folder_path", self.masks[idx])
+#         img = Image.open(img_path).convert("RGB")
+#         # note that we haven't converted the mask to RGB,
+#         # because each color corresponds to a different instance
+#         # with 0 being background
+#         mask = Image.open(mask_path)
 
-        mask = np.array(mask)
-        # instances are encoded as different colors
-        obj_ids = np.unique(mask)
-        # first id is the background, so remove it
-        obj_ids = obj_ids[1:]
+#         mask = np.array(mask)
+#         # instances are encoded as different colors
+#         obj_ids = np.unique(mask)
+#         # first id is the background, so remove it
+#         obj_ids = obj_ids[1:]
 
-        # split the color-encoded mask into a set
-        # of binary masks
-        masks = mask == obj_ids[:, None, None]
+#         # split the color-encoded mask into a set
+#         # of binary masks
+#         masks = mask == obj_ids[:, None, None]
 
-        # get bounding box coordinates for each mask
-        num_objs = len(obj_ids)
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
+#         # get bounding box coordinates for each mask
+#         num_objs = len(obj_ids)
+#         boxes = []
+#         for i in range(num_objs):
+#             pos = np.where(masks[i])
+#             xmin = np.min(pos[1])
+#             xmax = np.max(pos[1])
+#             ymin = np.min(pos[0])
+#             ymax = np.max(pos[0])
+#             boxes.append([xmin, ymin, xmax, ymax])
 
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
-        #masks = torch.as_tensor(masks, dtype=torch.uint8)
+#         boxes = torch.as_tensor(boxes, dtype=torch.float32)
+#         # there is only one class
+#         labels = torch.ones((num_objs,), dtype=torch.int64)
+#         #masks = torch.as_tensor(masks, dtype=torch.uint8)
 
-        image_id = torch.tensor([idx])
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+#         image_id = torch.tensor([idx])
+#         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+#         # suppose all instances are not crowd
+#         iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
-        target = {}
-        target["boxes"] = boxes
-        target["labels"] = labels
-        #target["masks"] = masks
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
+#         target = {}
+#         target["boxes"] = boxes
+#         target["labels"] = labels
+#         #target["masks"] = masks
+#         target["image_id"] = image_id
+#         target["area"] = area
+#         target["iscrowd"] = iscrowd
 
-        if self.transforms is not None:
-            img, target = self.transforms(img, target)
+#         if self.transforms is not None:
+#             img, target = self.transforms(img, target)
 
-        return img, target
+#         return img, target
 
-    def __len__(self):
-        return len(self.imgs)
+#     def __len__(self):
+#         return len(self.imgs)
